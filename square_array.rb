3534ea587e379ce16d2c132c4e5e45d645bce2f3
Kelsey Low <kklowlow@gmail.com>
2019-01-21 18:07:03 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,16 @@
+=begin
 def square_array(array)
   square_array = []
   array.each do |num|
     square_array << num * num
   end
   return square_array
+end
+=end
+
+def square_array(array)
+  array.collect do |num|
+    num * num
+  end
+  return array
 end
